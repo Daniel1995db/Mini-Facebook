36a3aacd78ca6d297ef83e1c5b96611b21b791af
MikeTeixeira <teixeiramichael5@gmail.com>
2017-10-25 19:36:24 -0400

    login/signup/allthat is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!([
-	{id: 1, fname: 'Pablo', lname: 'Escobar', username: 'donpablo', password: 'test', photo: open('http://static4.businessinsider.com/image/55fee5be9dd7cc11008bbaa4-480/pablo-escobar.jpg'), position: 'Leader' },
-	{id: 2, fname: 'Gustavo', lname: 'Gaviria', username: 'gg', password: 'test', photo: open('https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ3OTk4Njg2NV5BMl5BanBnXkFtZTgwMzIzMzk2NjE@._V1_SY1000_SX1500_AL_.jpg'), position: 'primo' }
+	{id: 1, fname: 'Pablo', lname: 'Escobar', username: 'donpablo', email:"pescobar@gmail.com", password: '123456', photo: open('http://static4.businessinsider.com/image/55fee5be9dd7cc11008bbaa4-480/pablo-escobar.jpg'), position: 'Leader' },
+	{id: 2, fname: 'Gustavo', lname: 'Gaviria', username: 'gg', email:"ggustavo@gmail.com", password: '123456', photo: open('https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ3OTk4Njg2NV5BMl5BanBnXkFtZTgwMzIzMzk2NjE@._V1_SY1000_SX1500_AL_.jpg'), position: 'primo' }
 ])
 Post.create([
 	{id: 1, message: 'Plata O Plomo', photo: open('http://pbs.twimg.com/media/CO_TUKkWUAAIN4o.jpg'), user_id: 1}
