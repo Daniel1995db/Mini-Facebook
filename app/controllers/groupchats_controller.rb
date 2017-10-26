05ffc3a5ca63052b5ec368d6ea4a4c5774a8794b
Daniel Bobadilla <DanielBobadilla1995@gmail.com>
2017-10-26 10:36:38 -0400

    user login restrictions, groups

--- app/controllers/groupchats_controller.rb
+++ app/controllers/groupchats_controller.rb
@@ -1,5 +1,6 @@
 class GroupchatsController < ApplicationController
   before_action :set_groupchat, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /groupchats
   # GET /groupchats.json
